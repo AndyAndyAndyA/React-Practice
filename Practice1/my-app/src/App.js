4a6706d9f58d599d365f21793748390beb45bcc8
andy <2397994870@qq.com>
2024-05-13 17:40:07 +0800

    commit

--- Practice1/my-app/src/App.js
+++ Practice1/my-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function fetchTodos() {
+  return [
+    {
+      id: 1,
+      title: '吃饭',
+      completed: false
+    },
+    {
+      id: 2,
+      title: '睡觉',
+      completed: false
+    },
+    // ... more todo items
+    {
+      id: 3,
+      title: '打豆豆',
+      completed: false
+    }
+  ]
 }
-
-export default App;
+export default function App() {
+  const todos = fetchTodos()
+  console.log(todos)
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => (
+          <li>
+            <input type='checkbox' checked={todo.completed} />
+            <label>{todo.title}</label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
