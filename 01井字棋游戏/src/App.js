616a0a320420e9cf3d155f051396a613212336ae
andy <2397994870@qq.com>
2024-06-01 10:03:33 +0800

    commit

--- 01井字棋游戏/src/App.js
+++ 01井字棋游戏/src/App.js
@@ -1,61 +1,27 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-// 方块组件
-function Square(props) {
+// 定义一个名为Square的组件，该组件接收一个名为value的属性，表示方格内的值。
+function Square({ value, onSquareClick }) {
   return (
-    <button className='square' onClick={props.onSquareClick}>
-      {props.value}
+    <button className='square' onClick={onSquareClick}>
+      {value}
     </button>
   )
 }
 
-// 胜利规则
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    // 如果 squares[a]、squares[b] 和 squares[c] 三个方块的值都相等，并且都不为 null，则返回这三个方块的值
-    // 否则，返回 null
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
-}
-
-// 游戏板组件
-function Board(props) {
-  // 第一个为x
-  const [xIsNext, setXIsNext] = useState(true)
-  // 使用 useState 创建一个状态变量 squares，初始值为一个包含 9 个 null 的数组
-  // 使用 setSquares 函数更新 squares 的值
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
-  // handleClick 函数会在用户点击方块时被调用
-  // 它将当前点击的方块的值设置为 'X'，并更新状态变量 squares
+// 定义一个名为Board的组件，该组件接收一个名为squares的属性，表示方格内的值数组。
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    // 如果已经有一个胜利者或者方块已经被点击，则返回
-    if (squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return
     }
-    if (squares[i] || calculateWinner(squares)) {
-      return
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'X'
+    } else {
+      nextSquares[i] = 'O'
     }
-    const newSquares = squares.slice()
-    newSquares[i] = xIsNext ? 'X' : 'O'
-    props.onPlay(newSquares)
-    setSquares(newSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares)
@@ -67,7 +33,7 @@ function Board(props) {
   }
 
   return (
-    <div className='board'>
+    <>
       <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -84,22 +50,22 @@ function Board(props) {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-    </div>
+    </>
   )
 }
 
-// 游戏组件
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0)
-  const currentSquares = history[currentMove]
   const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
   }
+
   function jumpTo(nextMove) {
     setCurrentMove(nextMove)
   }
@@ -128,4 +94,24 @@ export default function Game() {
       </div>
     </div>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
